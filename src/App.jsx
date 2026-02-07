692494339269068eae530f3df45e38ccda66a406
Stimur2304 <samatov.timur33@gmail.com>
2026-02-07 23:40:08 +0600

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Content from "./assets/Content/Content";
-import Final from "./assets/Content/finalPage";
+import ValentinesPage from "./assets/Content/finalPage";
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Content />} />
-      <Route path="/final" element={<Final />} />
+      <Route path="/final" element={<ValentinesPage />} />
     </Routes>
   );
 }
